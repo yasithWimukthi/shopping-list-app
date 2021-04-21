d6ba0d1b3cf5283fd70ce6c29943a8b1434e6f40
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-21 22:40:32 +0530

    create interface

--- app/src/main/java/com/ninjacode98/shoppinglist/MainActivity.java
+++ app/src/main/java/com/ninjacode98/shoppinglist/MainActivity.java
@@ -1,20 +1,30 @@
 package com.ninjacode98.shoppinglist;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder Builder;
+    private AlertDialog dialog;
+    private Button saveButton;
+    private EditText itemName;
+    private EditText itemQuantity;
+    private EditText itemColor;
+    private EditText itemSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @SuppressLint("ResourceType")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
