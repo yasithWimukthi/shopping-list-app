82e67c2ae4f780cfe7f163b0ee4dd8145b940479
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-22 12:10:31 +0530

    create DB Constants

---
+++ app/src/main/java/util/Constants.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Constants {
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "shoppingList";
+    public static final String TABLE_NAME = "shoppingItems";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_ITEM_NAME = "item";
+    public static final String COLUMN_ITEM_QUANTITY = "quantity";
+    public static final String COLUMN_COLOR = "color";
+    public static final String COLUMN_SIZE = "size";
+    public static final String COLUMN_DATE_ADDED = "dateAdded";
+
+}
